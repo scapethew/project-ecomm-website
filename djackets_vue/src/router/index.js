a57e135733671c8b31134da44f8d30bf44f3b2c4
Matthew Martin <matthew@hellois0.com>
2024-10-28 10:40:31 -0500

    Implemented a cart, ability to add to cart, loading bar/symbol while loading from server, set document/page titles, ability to view categories and converted some code to be reusable as a Vue component

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+
 const routes = [
   {
     path: '/',
@@ -14,7 +17,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/:category_slug/',
+    name: 'Category',
+    component: Category
+  },
 ]
 
 const router = createRouter({
